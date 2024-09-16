6d0ebe05625f640854dd6ea0b77190c3797ee1cf
hoangthien2510 <hngthien2000@gmail.com>
2024-09-16 10:26:15 +0700

    Implement delete post

--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { protectRoute } from '../middleware/protectRoute.js';
-import { createPost } from '../controllers/post.controller.js';
+import { createPost, deletePost } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
 router.post('/create', protectRoute, createPost);
+router.delete('/:id', protectRoute, deletePost);
 
 export default router;
