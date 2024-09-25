b1369e4af49fe110a2aa5f01d8f52920346995fe
hoangthien2510 <hngthien2000@gmail.com>
2024-09-25 19:33:16 +0700

    create UI homepage Sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,12 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/home/HomePage';
 import SignUpPage from './pages/auth/signup/SignUpPage';
 import LoginPage from './pages/auth/login/LoginPage';
+import Sidebar from './components/common/Sidebar';
 
 function App() {
   return (
     <div className='flex max-w-6xl mx-auto'>
+      <Sidebar />
       <Routes>
         <Route path='/' element={<HomePage />} />
         <Route path='/signup' element={<SignUpPage />} />
