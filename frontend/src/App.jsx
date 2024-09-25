6ab0bfe9a64141d26a7e8cd8495865771a9f4bae
hoangthien2510 <hngthien2000@gmail.com>
2024-09-25 19:41:35 +0700

    create UI homepage Right panel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import HomePage from './pages/home/HomePage';
 import SignUpPage from './pages/auth/signup/SignUpPage';
 import LoginPage from './pages/auth/login/LoginPage';
 import Sidebar from './components/common/Sidebar';
-
+import RightPanel from './components/common/RightPanel';
 function App() {
   return (
     <div className='flex max-w-6xl mx-auto'>
@@ -13,6 +13,7 @@ function App() {
         <Route path='/signup' element={<SignUpPage />} />
         <Route path='/login' element={<LoginPage />} />
       </Routes>
+      <RightPanel />
     </div>
   );
 }
