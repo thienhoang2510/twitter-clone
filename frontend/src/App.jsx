5b3983f6a1b7794862e233570b352acda258c373
hoangthien2510 <hngthien2000@gmail.com>
2024-10-04 08:33:44 +0700

    create UI Notifications page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import SignUpPage from './pages/auth/signup/SignUpPage';
 import LoginPage from './pages/auth/login/LoginPage';
 import Sidebar from './components/common/Sidebar';
 import RightPanel from './components/common/RightPanel';
+import NotificationPage from './pages/notification/NotificationPage';
+
 function App() {
   return (
     <div className='flex max-w-6xl mx-auto'>
@@ -12,6 +14,7 @@ function App() {
         <Route path='/' element={<HomePage />} />
         <Route path='/signup' element={<SignUpPage />} />
         <Route path='/login' element={<LoginPage />} />
+        <Route path='/notifications' element={<NotificationPage />} />
       </Routes>
       <RightPanel />
     </div>
